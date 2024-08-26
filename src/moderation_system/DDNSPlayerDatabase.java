096a5fd7754ada30479c8cf8c5a837c52c410457
Lett <lett@ubuntu-23.10-desktop>
2024-08-25 20:33:15 -0400

    Why does it say that these not pushed?

--- src/moderation_system/DDNSPlayerDatabase.java
+++ src/moderation_system/DDNSPlayerDatabase.java
@@ -1,18 +1,21 @@
 package moderation_system;
 
+import arc.util.Log;
 import mindustry.gen.Groups;
 import mindustry.gen.Player;
-
 import java.sql.*;
+import java.util.Random;
 
 public class DDNSPlayerDatabase {
     Connection databaseConnection;
+    private final Random randomGenerator = new Random();
+
     public DDNSPlayerDatabase() throws SQLException {
-        Connection connection = null;
+        Connection connection;
 
         try {
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:config/test.db");
+            connection = DriverManager.getConnection("jdbc:sqlite:config/players.db");
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() );
             return;
@@ -20,31 +23,43 @@ public class DDNSPlayerDatabase {
 
         this.databaseConnection = connection;
         Statement statement = connection.createStatement();
+        statement.executeUpdate("PRAGMA foreign_keys = ON;");
 
-        statement.executeUpdate("""
-                CREATE TABLE IF NOT EXISTS players
-                (
-                uuid        TEXT  PRIMARY KEY  NOT NULL,
-                last_ip     TEXT               NOT NULL,
-                last_name   TEXT               NOT NULL,
-                )
-                """);
-        statement.executeUpdate("""
-                CREATE TABLE IF NOT EXISTS banned_players
-                (
-                FOREIGN KEY (uuid) REFERENCES players(uuid)
-                ban_reason  TEXT               NOT NULL,
-                ban_start   INT                NOT NULL,
-                ban_end     INT                NOT NULL,
-                )
-                """);
+        statement.executeUpdate("CREATE TABLE IF NOT EXISTS players (" +
+                "uuid        TEXT  PRIMARY KEY  NOT NULL, " +
+                "last_ip     TEXT               NOT NULL, " +
+                "last_name   TEXT               NOT NULL" +
+                ");"
+        );
+
+        statement.executeUpdate("CREATE TABLE IF NOT EXISTS banned_players (" +
+                "uuid        TEXT               NOT NULL, " +
+                "ban_id      TEXT               NOT NULL, " +
+                "ban_reason  TEXT               NOT NULL, " +
+                "ban_start   INT                NOT NULL, " +
+                "ban_end     INT                NOT NULL, " +
+                "discord_id  TEXT               NOT NULL, " +
+                "FOREIGN KEY (uuid)       REFERENCES players(uuid), " +
+                "FOREIGN KEY (discord_id) REFERENCES staff(discord_id)" +
+                ");"
+        );
+
+        statement.executeUpdate("CREATE TABLE IF NOT EXISTS staff (" +
+                "discord_id  TEXT  PRIMARY KEY  NOT NULL, " +
+                "uuid        TEXT               NOT NULL, " +
+                "admin       INT                NOT NULL, " +
+                "FOREIGN KEY (uuid) REFERENCES players(uuid)" +
+                ");"
+        );
+
+
         statement.close();
     }
 
     public Player getPlayerFromBanID(String banID) throws SQLException {
         Statement statement = this.databaseConnection.createStatement();
         ResultSet databaseResult = statement.executeQuery(String.format("""
-                SELECT * WHERE ban_id == %s FROM banned_players
+                SELECT * FROM banned_players WHERE ban_id == %s;
                 """, banID
         ));
 
@@ -53,24 +68,26 @@ public class DDNSPlayerDatabase {
 
         statement.close();
 
-        Player player = Groups.player.find(p -> p.uuid().equals(uuid));
-        return player;
+        return Groups.player.find(p -> p.uuid().equals(uuid));
     }
 
     public Player getPlayerFromUUID(String UUID) throws SQLException {
         Statement statement = this.databaseConnection.createStatement();
-        ResultSet databaseResult = statement.executeQuery(String.format("""
-                SELECT * WHERE uuid == %s FROM players
-                """, UUID
+        ResultSet databaseResult = statement.executeQuery(String.format("SELECT * FROM players WHERE uuid = '%s';", UUID
         ));
 
-        String uuid = databaseResult.getString("uuid");
-        System.out.println(uuid);
+        String uuid;
+
+        if (databaseResult.next()) {
+            uuid = databaseResult.getString("uuid");
+        } else {
+            return null;
+        }
 
         statement.close();
+        databaseResult.close();
 
-        Player player = Groups.player.find(p -> p.uuid().equals(uuid));
-        return player;
+        return Groups.player.find(p -> p.uuid().equals(uuid));
     }
 
     public void addPlayer(String UUID, String lastIP, String lastName) throws SQLException {
@@ -81,9 +98,258 @@ public class DDNSPlayerDatabase {
             return;
         }
 
-        statement.executeUpdate(String.format("""
-                INSERT INTO players (uuid, last_ip, last_name) VALUES (%s, %s, %s);
-                """, UUID, lastIP, lastName
+        statement.executeUpdate(String.format("INSERT INTO players (uuid, last_ip, last_name) VALUES ('%s', '%s', '%s');",
+                UUID, lastIP, lastName
         ));
+        statement.close();
+    }
+
+    /**
+     * Gets a staff member via their UUID.
+     * @param UUID The UUID of the staff member.
+     * @return A Player object representing the staff member.
+     * @throws SQLException An exception that is thrown by SQL.
+     */
+    public Player getStaffFromUUID(String UUID) throws SQLException {
+        Statement statement = this.databaseConnection.createStatement();
+        ResultSet databaseResult = statement.executeQuery(String.format("SELECT * FROM staff WHERE uuid = '%s';", UUID
+        ));
+
+        String uuid;
+
+        if (databaseResult.next()) {
+            uuid = databaseResult.getString("uuid");
+        } else {
+            return null;
+        }
+
+        statement.close();
+        databaseResult.close();
+
+        return Groups.player.find(p -> p.uuid().equals(uuid));
+    }
+
+    /**
+     * Returns if a staff member has admin permissions.
+     * @param staff The Player object of the staff member.
+     * @return A Boolean object representing if the staff member has permissions. Returns null if the staff isn't in the
+     * database.
+     * @throws SQLException An exception that is thrown by SQL.
+     */
+    public Boolean hasAdminPermission(Player staff) throws SQLException {
+        Statement statement = this.databaseConnection.createStatement();
+        ResultSet databaseResult = statement.executeQuery(String.format("SELECT * FROM staff WHERE uuid = '%s';", staff.uuid()
+        ));
+
+        Boolean hasAdminPermission;
+
+        if (databaseResult.next()) {
+            hasAdminPermission = databaseResult.getInt("admin") == 1;
+        } else {
+            return null;
+        }
+
+        statement.close();
+        databaseResult.close();
+
+        return hasAdminPermission;
+    }
+
+    /**
+     * Gets a staff member via their Discord ID.
+     * @param discordID The Discord ID of the staff member.
+     * @return A Player object representing the staff member.
+     * @throws SQLException An exception that is thrown by SQL.
+     */
+    public Player getStaffFromDiscordID(String discordID) throws SQLException {
+        Statement statement = this.databaseConnection.createStatement();
+        ResultSet databaseResult = statement.executeQuery(String.format("SELECT * FROM staff WHERE discord_id = '%s';", discordID
+        ));
+
+        String uuid;
+
+        if (databaseResult.next()) {
+            uuid = databaseResult.getString("uuid");
+        } else {
+            return null;
+        }
+
+        statement.close();
+        databaseResult.close();
+
+        return Groups.player.find(p -> p.uuid().equals(uuid));
+    }
+
+    /**
+     * Gets a staff member's Discord ID via their UUID.
+     * @param UUID The UUID of the staff member.
+     * @return A String representing the staff member's Discord ID.
+     * @throws SQLException An exception that is thrown by SQL.
+     */
+    public String getStaffID(String UUID) throws SQLException {
+        Statement statement = this.databaseConnection.createStatement();
+        ResultSet databaseResult = statement.executeQuery(String.format("SELECT * FROM staff WHERE uuid = '%s';", UUID
+        ));
+
+        String discordID;
+
+        if (databaseResult.next()) {
+            discordID = databaseResult.getString("discord_id");
+        } else {
+            return null;
+        }
+
+        statement.close();
+        databaseResult.close();
+
+        return discordID;
+    }
+
+    /**
+     * Adds a staff member into the database.
+     * @param UUID The UUID of the staff member.
+     * @param admin_level A boolean representing if a staff member is an admin (has permission to ban).
+     * @throws SQLException An exception that is thrown by SQL.
+     */
+    public void addStaff(String UUID, Boolean admin_level, String discordID) throws SQLException {
+        Statement statement = this.databaseConnection.createStatement();
+
+        Player player = this.getStaffFromUUID(UUID);
+        if (player != null) {
+            Log.info("That staff member is already in the database.");
+            return;
+        }
+
+        statement.executeUpdate(String.format("INSERT INTO staff (uuid, admin, discord_id) VALUES ('%s', %s, '%s');",
+                UUID, admin_level.toString(), discordID
+        ));
+        statement.close();
+    }
+
+    public long getBanStartTime(String UUID) throws SQLException {
+        Statement statement = this.databaseConnection.createStatement();
+        ResultSet databaseResult = statement.executeQuery(String.format("""
+                SELECT * FROM banned_players WHERE uuid == '%s';
+                """, UUID
+        ));
+
+        long banStartTime = databaseResult.getLong("ban_end");
+
+        statement.close();
+
+        return banStartTime;
+    }
+
+    public long getBanEndTime(String UUID) throws SQLException {
+        Statement statement = this.databaseConnection.createStatement();
+        ResultSet databaseResult = statement.executeQuery(String.format("""
+                SELECT * FROM banned_players WHERE uuid == '%s';
+                """, UUID
+        ));
+
+        long banEndTime = 0;
+
+        if (databaseResult.next()) {
+            banEndTime = databaseResult.getLong("ban_end");
+        }
+
+        statement.close();
+        databaseResult.close();
+
+        return banEndTime;
+    }
+
+    public String getBanReason(String UUID) throws SQLException {
+        Statement statement = this.databaseConnection.createStatement();
+        ResultSet databaseResult = statement.executeQuery(String.format("""
+                SELECT * FROM banned_players WHERE uuid == '%s';
+                """, UUID
+        ));
+
+        String banReason = databaseResult.getString("ban_reason");
+
+        statement.close();
+
+        return banReason;
+    }
+
+    public String getBanID(String UUID) throws SQLException {
+        Statement statement = this.databaseConnection.createStatement();
+        ResultSet databaseResult = statement.executeQuery(String.format("""
+                SELECT * FROM banned_players WHERE uuid == '%s';
+                """, UUID
+        ));
+
+        String banEndTime = databaseResult.getString("ban_id");
+
+        statement.close();
+
+        return banEndTime;
+    }
+
+    /**
+     * @param UUID The UUID of the player to ban.
+     * @param banReason The reason for banning the player.
+     * @param endTime When the player's ban ends.
+     * @param staffID The Discord ID of the staff member that sent the ban.
+     * @throws SQLException An exception that is thrown by SQL.
+     */
+    public void addBan(String UUID, String banReason, String endTime, String staffID) throws SQLException {
+        long currentTime = System.currentTimeMillis();
+        Statement statement = this.databaseConnection.createStatement();
+
+//        Player player = this.getPlayerFromUUID(UUID);
+//        if (player != null) {
+//            return;  // TODO: Make this return a specific value.
+//        }
+
+        boolean generateNewBanID = true;
+        String hexBanID = "";
+
+        while (generateNewBanID) {
+            long newBanID = randomGenerator.nextLong() + (1L << 61);
+            hexBanID = Long.toHexString(newBanID);
+
+//            ResultSet databaseResult = statement.executeQuery(String.format("""
+//                SELECT * FROM banned_players WHERE ban_id == '%s';
+//                """, hexBanID
+//            ));
+//            databaseResult.close();
+            generateNewBanID = false;
+        }
+
+        System.out.println(hexBanID);
+
+        statement.executeUpdate(String.format("""
+                INSERT INTO banned_players (uuid, ban_id, ban_reason, ban_start, ban_end, discord_id) VALUES ('%s', '%s',
+                 '%s', '%s', '%s', '%s');
+               \s""", UUID, hexBanID, banReason, currentTime, endTime, staffID
+        ));
+        statement.close();
+    }
+
+    /**
+     * Checks if a ban is still active.
+     * @param UUID The UUID of the player.
+     * @return true if the ban is still active, false if it isn't active.
+     * @throws SQLException SQLException.
+     */
+    public boolean checkBan(String UUID) throws SQLException {
+        long currentTime = System.currentTimeMillis();
+        long banEndTime = this.getBanEndTime(UUID);
+        boolean banIsValid = currentTime <= banEndTime;
+
+        if (!banIsValid) {
+            Statement statement = this.databaseConnection.createStatement();
+
+            statement.executeUpdate(String.format("""
+                DELETE FROM banned_players WHERE UUID=='%s';
+                """, UUID
+            ));
+
+            statement.close();
+        }
+
+        return banIsValid;
     }
 }
--- src/moderation_system/Main.java
+++ src/moderation_system/Main.java
@@ -1,7 +1,257 @@
 package moderation_system;
 
+import arc.Events;
+import arc.util.CommandHandler;
+import arc.util.Log;
+import mindustry.game.EventType;
+import mindustry.gen.Groups;
+import mindustry.gen.Player;
 import mindustry.mod.Plugin;
 
-public class Main extends Plugin {
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
 
+import static java.lang.Integer.parseInt;
+
+public class Main extends Plugin {
+    public final String pluginMessageName = "[gray]<[#003ec8]DDNS Moderation System[gray]>[white] ";
+    private final Map<String, String> playerIdentifiers = new HashMap<String, String>();
+    private final Random randomGenerator = new Random();
+    private final DDNSPlayerDatabase database;
+
+    {
+        try {
+            database = new DDNSPlayerDatabase();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+    /**
+     * @param adminLevel whether to only allow up to admin level.
+     * @return A boolean representing the player's permission to execute the command.
+     */
+    public Boolean checkPermission(Boolean adminLevel, String UUID) throws SQLException {
+        Player player = database.getPlayerFromUUID(UUID);
+        Boolean hasDatabaseAdmin = database.hasAdminPermission(player);
+
+        if (hasDatabaseAdmin == null) {
+            return false;
+        }
+
+        Log.info(hasDatabaseAdmin);
+        return hasDatabaseAdmin || !adminLevel;
+    }
+
+    // https://stackoverflow.com/a/2904266
+    public static <T, E> T getKeyByValue(Map<T, E> map, E value) {
+        for (Map.Entry<T, E> entry : map.entrySet()) {
+            if (Objects.equals(value, entry.getValue())) {
+                return entry.getKey();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void init(){
+        Events.on(EventType.PlayerJoin.class, event -> {
+            long newPlayerID = randomGenerator.nextInt() + (1L << 31);
+            String hexPlayerID = Long.toHexString(newPlayerID);
+            boolean banned;
+
+//            event.player.sendMessage(pluginMessageName + hexPlayerID);
+            playerIdentifiers.put(event.player.uuid(), hexPlayerID);
+
+            try {
+                database.addPlayer(event.player.uuid(), event.player.ip(), event.player.name());
+                banned = database.checkBan(event.player.uuid());
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            if (banned) {
+                String banReason = null;
+                String banID = null;
+
+                try {
+                    banReason = database.getBanReason(event.player.uuid());
+                    banID = database.getBanID(event.player.uuid());
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+
+                String banMessage = String.format("""
+                        [scarlet]You are banned from this server.
+                        [orange]Reason[gray]:[white] %s
+                        
+                        [orange]Ban ID[gray]:[white] %s""", banReason, banID
+                );
+                event.player.kick(banMessage, 0);
+            }
+        });
+    }
+
+    @Override
+    public void registerClientCommands(CommandHandler handler) {
+        handler.<Player>register("kick", "<id> <reason...>", "Kick a player.", (args, player) -> {
+            String id = args[0];
+            String reason = args[1];
+            String playerUUID = getKeyByValue(playerIdentifiers, id);
+
+            try {
+                if (!checkPermission(false, player.uuid())) {
+                    player.sendMessage(pluginMessageName + "You do not have permission to run this command.");
+                    return;
+                }
+            } catch (SQLException e) {
+                System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                return;
+            }
+
+            Player playerToKick = Groups.player.find(p -> p.uuid().equals(playerUUID));
+
+            if(playerToKick == null){
+                player.sendMessage(pluginMessageName + "[scarlet]Could not find a player by that ID.");
+                return;
+            }
+
+            playerToKick.kick(reason);
+        });
+
+        handler.<Player>register("warn", "<id> <reason...>", "Warn a player.", (args, player) -> {
+            String id = args[0];
+            String reason = args[1];
+            String playerUUID = getKeyByValue(playerIdentifiers, id);
+
+            try {
+                if (!checkPermission(false, player.uuid())) {
+                    player.sendMessage(pluginMessageName + "You do not have permission to run this command.");
+                    return;
+                }
+            } catch (SQLException e) {
+                System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                return;
+            }
+
+            Player playerToWarn = Groups.player.find(p -> p.uuid().equals(playerUUID));
+
+            if(playerToWarn == null){
+                player.sendMessage(pluginMessageName + "[scarlet]Could not find a player by that ID.");
+                return;
+            }
+
+            player.sendMessage(pluginMessageName + "[scarlet]WARNING[gray]:[white] " + reason);
+        });
+
+        handler.<Player>register("info", "<username...>", "Get the info of a player.", (args, player) -> {
+            String username = args[0];
+
+            try {
+                if (!checkPermission(false, player.uuid())) {
+                    player.sendMessage(pluginMessageName + "You do not have permission to run this command.");
+                    return;
+                }
+            } catch (SQLException e) {
+                System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                return;
+            }
+
+            Player targetPlayer = Groups.player.find(p -> p.plainName().equalsIgnoreCase(username));
+
+            if (targetPlayer == null) {
+                player.sendMessage(pluginMessageName + "[scarlet]Could not find a player by that username.");
+                return;
+            }
+
+            String targetPlayerID = playerIdentifiers.get(targetPlayer.uuid());
+
+            String infoMessage = String.format("""
+                    ID: %s
+                    IP: %s
+                    Last Name Used: %s[white]
+                    Is admin: %b
+                    """,
+                    targetPlayerID,
+                    targetPlayer.ip(),
+                    targetPlayer.name(),
+                    targetPlayer.admin()
+            );
+            player.sendMessage(infoMessage);
+        });
+
+        handler.<Player>register("ban", "<id> <duration> <reason...>", "Ban a player.",
+                (args, player) -> {
+            String id = args[0];
+            long duration = (long) parseInt(args[1]);
+            String reason = args[2];
+
+            long durationMillis = duration * 86400000;
+
+            long currentTime = System.currentTimeMillis();
+            long endTime = currentTime + durationMillis;
+
+            try {
+                if (!checkPermission(true, player.uuid())) {
+                    player.sendMessage(pluginMessageName + "You do not have permission to run this command.");
+                    return;
+                }
+            } catch (SQLException e) {
+                System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+                return;
+            }
+
+            String staffID = null;
+            try {
+                staffID = database.getStaffID(player.uuid());
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            String playerUUID = getKeyByValue(playerIdentifiers, id);
+            Player playerToBan = Groups.player.find(p -> p.uuid().equals(playerUUID));
+
+            if (playerToBan == null) {
+                player.sendMessage(pluginMessageName + "[scarlet]Could not find a player by that ID.");
+                return;
+            }
+
+            try {
+                database.addBan(playerUUID, reason, String.valueOf(endTime), staffID);
+                String banID = database.getBanID(playerUUID);
+
+                String banMessage = String.format("""
+                        [scarlet]You are banned from this server.
+                        [orange]Reason[gray]:[white] %s
+                        
+                        [orange]Ban ID[gray]:[white] %s""", reason, banID
+                );
+                playerToBan.kick(banMessage, 0);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
+
+    @Override
+    public void registerServerCommands(CommandHandler handler) {
+        handler.register("add_staff", "<uuid> <discord_id> <admin>", "Adds a new staff member. Set" +
+                " admin to true if they have the ability to ban and unban.", args -> {
+            String UUID = args[0];
+            String discordID = args[1];
+            boolean admin = args[2].equals("true") || args[2].equals("1");
+
+            try {
+                database.addStaff(UUID, admin, discordID);
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+            Log.info("Staff added.");
+        });
+    }
 }
