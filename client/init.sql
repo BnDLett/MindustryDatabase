1374c7b91ea452347ed66fc88af71d0302bb446e
xasmedy <xasmedy08cim@gmail.com>
2025-01-04 20:43:09 +0100

    Table primary key data type changed.

--- client/init.sql
+++ client/init.sql
@@ -2,58 +2,61 @@
 
 CREATE TABLE IF NOT EXISTS account(
 
-    id               INT(11)             NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    username         VARCHAR(32)         NOT NULL,
+    id               INT UNSIGNED        NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username         VARCHAR(32)         NOT NULL UNIQUE,
     account_creation BIGINT(20) UNSIGNED NOT NULL,
     salt             BINARY(16),         NOT NULL,
-    password         BINARY(128),        NOT NULL
-    
-    CONSTRAINT u_account_username UNIQUE(username)
+    password         BINARY(128),        NOT NULL    
 );
 
 CREATE TABLE IF NOT EXISTS ip_address(
 
-    id                INT(11)             NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    ip                VARCHAR(15)         NOT NULL,
+    id                INT UNSIGNED        NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ip                VARCHAR(15)         NOT NULL UNIQUE,
     registration_date BIGINT(20) UNSIGNED NOT NULL,
-    blacklisted       BOOLEAN             NOT NULL,
-    
-    CONSTRAINT u_ip_address_ip UNIQUE(ip)
+    blacklisted       BOOLEAN             NOT NULL,    
 );
 
 CREATE TABLE IF NOT EXISTS account_login(
 
-    id              INT(11)             NOT NULL PRIMARY KEY AUTO_INCREMENT ,
-    account_id      INT(11)             NOT NULL,
-    ip_id           INT(11)             NOT NULL,
-    session         BINARY(32),         NULL -- TODO Cannot be null, move to a session table?
-    login_date      BIGINT(20) unsigned NOT NULL,
-    expiration_date BIGINT(20) unsigned NOT NULL,
+    id              INT UNSIGNED        NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    account_id      INT UNSIGNED        NOT NULL,
+    ip_id           INT UNSIGNED        NOT NULL,
+    login_date      BIGINT(20) UNSIGNED NOT NULL,
+    expiration_date BIGINT(20) UNSIGNED NOT NULL,
 
-    CONSTRAINT u_logins_session UNIQUE(session),
     CONSTRAINT fk_logins_user   FOREIGN KEY(account_id) REFERENCES account(id),
     CONSTRAINT fk_logins_ip     FOREIGN KEY(ip_id)      REFERENCES ip_address(id)
 );
 
+CREATE TABLE IF NOT EXISTS account_session(
+
+    id             INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    account_id     INT UNSIGNED NOT NULL,
+    session_cookie BINARY(32)   NOT NULL UNIQUE,
+
+    CONSTRAINT fk_account_session REFERENCES account(id)
+);
+
 CREATE TABLE IF NOT EXISTS server(
 
-    id        INT(11)      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id        INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     ip        VARCHAR(15)  NOT NULL,
     port      INT(5)       NOT NULL,
     name      VARCHAR(100) NOT NULL,
     last_ping BIGINT(20) unsigned DEFAULT 0,
 
-    CONSTRAINT u_servers_ip_port UNIQUE(ip, port)
+    CONSTRAINT u_server_ip_port UNIQUE(ip, port)
 );
 
 -- TODO To discuss
 CREATE TABLE IF NOT EXISTS user_joins(
 
-    id           INT(11)             NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    account_id   INT(11)             NOT NULL,
-    server_id    INT(11)             NOT NULL,
-    join_date    BIGINT(20) unsigned NOT NULL,
-    leave_date   BIGINT(20) unsigned NOT NULL DEFAULT 0,
+    id           INT UNSIGNED        NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    account_id   INT UNSIGNED        NOT NULL,
+    server_id    INT UNSIGNED        NOT NULL,
+    join_date    BIGINT(20) UNSIGNED NOT NULL,
+    leave_date   BIGINT(20) UNSIGNED NOT NULL DEFAULT 0,
 
     CONSTRAINT fk_joins_user   FOREIGN KEY(account_id) REFERENCES account(id),
     CONSTRAINT fk_joins_server FOREIGN KEY(server_id)  REFERENCES server(id)
@@ -61,28 +64,27 @@ CREATE TABLE IF NOT EXISTS user_joins(
 
 CREATE TABLE IF NOT EXISTS ban(
 
-    id              INT(11)      NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    account_id      INT(11)      NOT NULL,
-    staff_id        INT(11)      NOT NULL,
-    where_issued_id INT(11)      NOT NULL,
-    reason          VARCHAR(256) NOT NULL,
+    id              INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    account_id      INT UNSIGNED NOT NULL,
+    staff_id        INT UNSIGNED NOT NULL,
+    where_issued_id INT UNSIGNED NOT NULL, -- TODO Needed?
+    reason          VARCHAR(256) NOT NULL, -- TODO Check for memory reason if 255 is better.
     when_issued     BIGINT(20)   NOT NULL,
-    expiration_date BIGINT(20), -- TODO Not sure about this one
+    expiration_date BIGINT(20), -- TODO Use null for permanent bans?
 
-    CONSTRAINT fk_bans_user         FOREIGN KEY(account_id)      REFERENCES account(id),
-    CONSTRAINT fk_bans_staff        FOREIGN KEY(staff_id)        REFERENCES account(id),
-    CONSTRAINT fk_bans_where_issued FOREIGN KEY(where_issued_id) REFERENCES server(id)
+    CONSTRAINT fk_ban_user         FOREIGN KEY(account_id)      REFERENCES account(id),
+    CONSTRAINT fk_ban_staff        FOREIGN KEY(staff_id)        REFERENCES account(id),
+    CONSTRAINT fk_ban_where_issued FOREIGN KEY(where_issued_id) REFERENCES server(id)
 );
 
 CREATE TABLE IF NOT EXISTS unban(
 
-    id              INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    ban_id          INT(11) NOT NULL,
-    staff_id        INT(11) NOT NULL,
-    where_issued_id INT(11) NOT NULL,
-    when_issued     BIGINT(20) NOT NULL,
+    id              INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    ban_id          INT UNSIGNED NOT NULL UNIQUE,
+    staff_id        INT UNSIGNED NOT NULL,
+    where_issued_id INT UNSIGNED NOT NULL, -- TODO Needed?
+    when_issued     BIGINT(20)   NOT NULL,
 
-    CONSTRAINT u_unbans_ban           UNIQUE(ban_id),
     CONSTRAINT fk_unbans_ban          FOREIGN KEY(ban_id)          REFERENCES ban(id),
     CONSTRAINT fk_unbans_staff        FOREIGN KEY(staff_id)        REFERENCES account(id),
     CONSTRAINT fk_unbans_where_issued FOREIGN KEY(where_issued_id) REFERENCES server(id)
@@ -90,11 +92,11 @@ CREATE TABLE IF NOT EXISTS unban(
 
 CREATE TABLE IF NOT EXISTS kick(
 
-    id              INT(11)      NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    account_id      INT(11)      NOT NULL,
-    staff_id        INT(11)      NOT NULL,
-    server_id       INT(11)      NOT NULL,
-    where_issued_id INT(11)      NOT NULL,
+    id              INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    account_id      INT UNSIGNED NOT NULL,
+    staff_id        INT UNSIGNED NOT NULL,
+    server_id       INT UNSIGNED NOT NULL,
+    where_issued_id INT UNSIGNED NOT NULL, -- TODO Needed?
     reason          VARCHAR(256) NOT NULL,
     when_issued     BIGINT(20)   NOT NULL,
 
@@ -106,10 +108,10 @@ CREATE TABLE IF NOT EXISTS kick(
 
 CREATE TABLE IF NOT EXISTS warn(
 
-    id              INT(11)      NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    account_id      INT(11)      NOT NULL,
-    staff_id        INT(11)      NOT NULL,
-    where_issued_id INT(11)      NOT NULL,
+    id              INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    account_id      INT UNSIGNED NOT NULL,
+    staff_id        INT UNSIGNED NOT NULL,
+    where_issued_id INT UNSIGNED NOT NULL, -- TODO Needed?
     reason          VARCHAR(256) NOT NULL,
     when_issued     BIGINT(20)   NOT NULL,
     received        BOOLEAN      NOT NULL,
@@ -121,45 +123,43 @@ CREATE TABLE IF NOT EXISTS warn(
 
 CREATE TABLE IF NOT EXISTS role(
 
-    id    INT(11)      NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id    INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name  VARCHAR(256) NOT NULL,
-    color char(8)      NOT NULL,
+    color CHAR(8)      NOT NULL,
     level INT(3)       NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS users_roles(
+CREATE TABLE IF NOT EXISTS account_role(
 
-    id         INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    account_id INT(11) NOT NULL,
-    role_id    INT(11) NOT NULL,
+    id         INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    account_id INT UNSIGNED NOT NULL,
+    role_id    INT UNSIGNED NOT NULL,
 
     CONSTRAINT fk_roles_user FOREIGN KEY(account_id) REFERENCES account(id),
     CONSTRAINT fk_roles_role FOREIGN KEY(role_id)    REFERENCES role(id)
 );
 
 -- TODO Integrate inside of the role table instead?
-CREATE TABLE IF NOT EXISTS symbols(
+CREATE TABLE IF NOT EXISTS symbol(
 
-    id     INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    symbol char(1) NOT NULL,
+    id     INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    symbol CHAR(1)      NOT NULL,
 
     CONSTRAINT u_symbols_symbol UNIQUE(symbol)
 );
 
 CREATE TABLE IF NOT EXISTS permission(
 
-    id       INT(11)      NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    property VARCHAR(200) NOT NULL,
+    id       INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    property VARCHAR(200) NOT NULL UNIQUE,
     name     VARCHAR(100) NOT NULL, -- TODO Remove and only use the property?
-
-    CONSTRAINT u_permissions_property UNIQUE(property)
 );
 
 CREATE TABLE IF NOT EXISTS role_permission(
 
-    id            INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    role_id       INT(11) NOT NULL,
-    permission_id INT(11) NOT NULL,
+    id            INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id       INT UNSIGNED NOT NULL,
+    permission_id INT UNSIGNED NOT NULL,
 
     CONSTRAINT fk_role       FOREIGN KEY(role_id)       REFERENCES role(id),
     CONSTRAINT fk_permission FOREIGN KEY(permission_id) REFERENCES permission(id)
